b999f897228f8729c10656a363067e1c61276912
koushik0004 <koushik.sadhukhan@sapient.com>
2019-12-06 22:51:46 +0530

    removing build folder

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ExampleServer_1 = require("./ExampleServer");
-const exampleServer = new ExampleServer_1.default();
-exampleServer.start(3001);
